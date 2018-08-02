c721b6a2730686c956b8d0f988eacc32978f7f97
DESKTOP-U772PUB\Azam <azam.shirani@gmail.com>
2018-08-01 22:31:09 -0400

    DAY_03(value/comparison/operators)

--- Nj-javascript/index.js
+++ Nj-javascript/index.js
@@ -1,6 +1,36 @@
-let total = 0, count = 1;
-while (count <= 10) {
-total += count;
-count += 1;
-}
-console.log(total);
+console.log(typeof 4.5)
+console.log(- (10 - 2))
+console.log(3 > 2)
+// → true
+console.log(3 < 2)
+// → false
+console.log("Aardvark" < "Zoroaster")
+console.log("a" != "b")
+console.log("a" == "b")
+console.log("Itchy" != "Scratchy")
+// → true
+console.log("Apple" == "Orange")
+// → false
+//There is only one value in JavaScript that is not equal to itself, and that is NaN (“not a number”).
+console.log(NaN == NaN)
+// . false
+//JavaScript supports three logical operators: and, or, and not.
+console.log(true && false)
+// . false
+console.log(true && true)
+// . true
+console.log(false || true)
+// . true
+console.log(false || false)
+// . false
+//Not is written as an exclamation mark (!).
+console.log(!true)
+//.false
+//Ternary operator or coonditional operator
+console.log(true ? 1 : 2);
+// . 1
+console.log(false ? 1 : 2);
+// . 2
+
+
+
