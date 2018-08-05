aa53a24edbb17434e2996bba1fe77f3679fb1995
DESKTOP-U772PUB\Azam <azam.shirani@gmail.com>
2018-08-05 19:59:11 -0400

    Day_05

---
+++ Js-basic/Day_05/Loops.js
@@ -0,0 +1,60 @@
+//while loop
+let number = 0;
+while(number <= 12){
+    console.log(number)
+    number+=2;
+}
+
+//a do loop always executes its body at least once.
+
+    var text = " "
+    var i = 0;
+    do {
+        text += "The number is " + i  ;
+        i++;
+    }
+    while (i < 5);
+    console.log(text);
+    
+
+
+//Indenting Code
+if (false != true) {
+console.log("That makes sense.");
+if (1 < 2) {
+console.log("No surprise there.");
+}
+}
+
+//for loop
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+result = result * 2;
+}
+console.log(result);
+// ? 1024
+
+for (let courrent = 20; ; courrent = courrent + 1){
+ if(courrent % 7 == 0){
+   console.log(courrent)
+   break;
+  }
+}
+
+//switch case loop
+switch("sunny"){
+case "rainy":
+ console.log("Please bring an umbrella");
+ break;
+case "cloudy":
+break;
+console.log("dress lightly");
+case "sunny":
+console.log("Put on your sunglass");
+break;
+default:
+console.log("Unknown weather type!");
+break;
+
+}
+  
---
+++ Js-basic/Day_05/values.js
@@ -0,0 +1,13 @@
+//binding or variable:
+let ten = 10;
+console.log(ten * ten);
+// ? 100
+let one = 3 ,two = 5;
+console.log(one + two);
+//8
+//The words var and const can also be used to create bindings, in a way similar to let
+var name = "Ayda";
+const greeting = "Hello ";
+console.log(greeting + name);
+// ? Hello Ayda
+console.log(Math.min(2,8) + 100);
