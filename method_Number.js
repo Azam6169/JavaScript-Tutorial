7916c34e4735de466aad524ff3964c1ad980e3dc
DESKTOP-U772PUB\Azam <azam.shirani@gmail.com>
2018-08-06 22:55:52 -0400

    methods

---
+++ method_Number.js
@@ -0,0 +1,8 @@
+
+//toString() returns a number as a string
+//All number methods can be used on any type of numbers (literals, variables, or expressions):
+var x = 123;
+x.toString();            // returns 123 from variable x
+(123).toString();        // returns 123 from literal 123
+(100 + 23).toString();   // returns 123 from expression 100 + 23
+console.log(x);
