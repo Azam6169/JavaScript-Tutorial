dbb05a7110f6c49c36888934f108763db39eae82
Alejendro-pc\Alejendro <azam.shirani@gmail.com>
2018-09-08 22:48:19 -0400

    Update _Array

--- Array.js
+++ Array.js
@@ -9,6 +9,18 @@ console.log(cars.length);
 
 console.log(cars.sort());
 
+var points = [40, 100, 1, 5, 25, 10];
+console.log(points.sort());
+console.log(points[4]);
+
+//type of
+//The typeof operator returns object because a JavaScript array is an object.
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(typeof fruits);
+
+
+
+
 
 
 
